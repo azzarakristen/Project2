68c8a62cf0c6df4c1bd5994eafced445ae34a792
azzarakristen <azzak835@newschool.edu>
2024-03-26 14:28:07 -0400

    progress

--- Project_2/script.js
+++ Project_2/script.js
@@ -1,46 +1,453 @@
 const data = [
-{
-    "name": "Leo Dominguez",
-    "arms": 2,
-    "legs": 2,
-    "nose": 1,
-    "hat": 1,
-    "shellColor": "n/a",
-    "picture": "none"
-},
-{
-    "name": "Mr. Snail",
-    "arms": 0,
-    "legs": 0,
-    "nose": 0,
-    "hat": 1,
-    "shellColor": "cyan",
-    "picture": "https://img.pixers.pics/pho_wat(s3:700/FO/63/25/88/41/700_FO63258841_82d512c8bae8a9b5eb726314987582d3.jpg,700,617,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,480,567,jpg)/posters-cute-snail-cartoon.jpg.jpg"
-}
-]
+    {
+        "name": "MOUNT MARCY",
+        "elevation": 5344,
+        "ascent": 3166,
+        "route": 14.8,
+        "difficulty": 5,
+        "picture": "images/marcy.jpg",
+        "topo": "images/marcytopo.jpg",
+    },
+        {
+        "name": "ALGONQUIN PEAK",
+        "elevation": 5114,
+        "ascent": 2936,
+        "route": 9.6,
+        "difficulty": 5,
+        "picture": "images/algonquin.jpg",
+        "topo": "images/algonquintopo.jpg",
 
-window.addEventListener('load', ()=> {
+    },
+    {
+        "name": "MOUNT HAYSTACK",
+        "elevation": 4960,
+        "ascent": 3570,
+        "route": 17.8,
+        "difficulty": 7,
+        "picture": "images/haystack.jpg",
+        "topo": "images/haystacktopo.png"
+
+    },
+    {
+        "name": "MOUNT SKYLIGHT",
+        "elevation": 4926,
+        "ascent": 4265,
+        "route": 17.9,
+        "difficulty": 7,
+        "picture": "images/skylight.jpg",
+        "topo": "images/skylighttopo.png",
+
+    },
+    {
+        "name": "WHITEFACE MOUNTAIN",
+        "elevation": 4867,
+        "ascent": 2535,
+        "route": 10.4,
+        "difficulty": 4,
+
+    },
+    {
+        "name": "DIX MOUNTAIN",
+        "elevation": 4857,
+        "ascent": 2800,
+        "route": 13.2,
+        "difficulty": 7,
+
+    },
+    {
+        "name": "GRAY PEAK",
+        "elevation": 4840,
+        "ascent": 4178,
+        "route": 16,
+        "difficulty": 7,
+
+    },
+    {
+        "name": "IRIQUOIS PEAK",
+        "elevation": 4840,
+        "ascent": 3250,
+        "route": 11.6,
+        "difficulty": 6,
+
+    },
+    {
+        "name": "BASIN MOUNTAIN",
+        "elevation": 4827,
+        "ascent": 3650,
+        "route": 16.5,
+        "difficulty": 6,
+
+    },
+    {
+        "name": "GOTHICS",
+        "elevation": 4736,
+        "ascent": 4070,
+        "route": 10,
+        "difficulty": 5,
+
+    },
+    {
+        "name": "MOUNT COLDEN",
+        "elevation": 4714,
+        "ascent": 2850,
+        "route": 15.2,
+        "difficulty": 5,
+        
+    },
+    {
+        "name": "GIANT MOUNTAIN",
+        "elevation": 4627,
+        "ascent": 3050,
+        "route": 6,
+        "difficulty": 4,
+        "picture": "images/giant.jpg",
+        "topo": "images/gianttopo.jpg",
+        
+    },
+    {
+        "name": "NIPPLETOP",
+        "elevation": 4620,
+        "ascent": 4050,
+        "route": 12.6,
+        "difficulty": 5,
+    
+    },
+    {
+        "name": "SANTANONI PEAK",
+        "elevation": 4607,
+        "ascent": 2860,
+        "route": 11.4,
+        "difficulty": 5,
+
+    },
+    {
+        "name": "MOUNT REDFIELD",
+        "elevation": 4606,
+        "ascent": 3225,
+        "route": 17.5,
+        "difficulty": 6,
+
+    },
+    {
+        "name": "WRIGHT PEAK",
+        "elevation": 4580,
+        "ascent": 2400,
+        "route": 7,
+        "difficulty": 4,
+
+    },
+    {
+        "name": "SADDLEBACK MOUNTAIN",
+        "elevation": 4515,
+        "ascent": 2990,
+        "route": 13.4, 
+        "difficulty": 5,
+    
+    },
+    {
+        "name": "PANTHER PEAK",
+        "elevation": 4442,
+        "ascent": 3762,
+        "route": 12,
+        "difficulty": 6,
+
+    },
+    {
+        "name": "TABLE TOP MOUNTAIN",
+        "elevation": 4427,
+        "ascent": 3660,
+        "route": 10,
+        "difficulty": 5,
+
+    },
+    {
+        "name": "ROCKY PEAK RIDGE",
+        "elevation": 4420,
+        "ascent": 4500,
+        "route": 13.4,
+        "difficulty": 5,
+
+    },
+    {
+        "name": "MACOMB MOUNTAIN",
+        "elevation": 4405,
+        "ascent": 2344,
+        "route": 8.4,
+        "difficulty": 5,
+
+    },
+    {
+        "name": "ARMSTRONG MOUNTAIN",
+        "elevation": 4400,
+        "ascent": 3734,
+        "route": 12.7,
+        "difficulty": 5,
+
+    },
+    {
+        "name": "HOUGH PEAK",
+        "elevation": 4400,
+        "ascent": 3200,
+        "route": 13.7,
+        "difficulty": 6,
+
+    },
+    {
+        "name": "SEWARD MOUNTAIN",
+        "elevation": 4361,
+        "ascent": 3490,
+        "route": 16,
+        "difficulty": 7,
+    
+    },
+    {
+        "name": "MOUNT MARSHALL",
+        "elevation": 4360,
+        "ascent": 2575,
+        "route": 14,
+        "difficulty": 6,
+
+    },
+    {
+        "name": "ALLEN MOUNTAIN",
+        "elevation": 4340,
+        "ascent": 2540,
+        "route": 16.2,
+        "difficulty": 7,
+
+    },
+    {
+        "name": "BIG SLIDE MOUNTAIN",
+        "elevation": 4240,
+        "ascent": 2800,
+        "route": 9.4,
+        "difficulty": 4,
+
+    },
+    {
+        "name": "ESTHER MOUNTAIN",
+        "elevation": 4240,
+        "ascent": 3020,
+        "route": 9.4,
+        "difficulty": 4,
+
+    },
+    {
+        "name": "UPPER WOLFJAW",
+        "elevation": 4185,
+        "ascent": 3619,
+        "route": 12.7,
+        "difficulty": 5,
+    
+    },
+    {
+        "name": "LOWER WOLFJAW",
+        "elevation": 4175,
+        "ascent": 2825,
+        "route": 8.7,
+        "difficulty": 4,
+
+    },
+    {
+        "name": "STREET MOUNTAIN",
+        "elevation": 4166,
+        "ascent": 2115,
+        "route": 8.8,
+        "difficulty": 6,
+
+    },
+    {
+        "name": "PHELPS MOUNTAIN",
+        "elevation": 4161,
+        "ascent": 1982,
+        "route": 8.8,
+        "difficulty": 5,
+    
+    },
+    {
+        "name": "MOUNT DONALDSON",
+        "elevation": 4140,
+        "ascent": 3490,
+        "route": 17,
+        "difficulty": 7,
+    
+    },
+    {
+        "name": "SEYMOUR MOUNTAIN",
+        "elevation": 4120,
+        "ascent": 2370,
+        "route": 14,
+        "difficulty": 6,
+
+    },
+    {
+        "name": "SAWTEETH",
+        "elevation": 4100,
+        "ascent": 2975,
+        "route": 11.8,
+        "difficulty": 4,
+
+    },
+    {
+        "name": "CASCADE MOUNTAIN",
+        "elevation": 4098,
+        "ascent": 1940,
+        "route": 4.8,
+        "difficulty": 2,
+
+    },
+    {
+        "name": "SOUTH DIX",
+        "elevation": 4060,
+        "ascent": 3050,
+        "route": 11.5,
+        "difficulty": 6,
+
+    },
+    {
+        "name": "PORTER MOUNTAIN",
+        "elevation": 4059,
+        "ascent": 2700,
+        "route": 7.6,
+        "difficulty": 3,
+
+    },
+    {
+        "name": "MOUNT COLVIN",
+        "elevation": 4057,
+        "ascent": 2130,
+        "route": 10.8,
+        "difficulty": 4,
+
+    },
+    {
+        "name": "MOUNT EMMONS",
+        "elevation": 4040,
+        "ascent": 3490,
+        "route": 18,
+        "difficulty": 7,
+
+    },
+    {
+        "name": "DIAL MOUNTAIN",
+        "elevation": 4020,
+        "ascent": 3450,
+        "route": 10,
+        "difficulty": 5,
+
+    },
+    {
+        "name": "GRACE PEAK",
+        "elevation": 4012,
+        "ascent": 3002,
+        "route": 12.5,
+        "difficulty": 6,
+
+    },
+    {
+        "name": "BLAKE",
+        "elevation": 3960,
+        "ascent": 3270,
+        "route": 13.6,
+        "difficulty": 4,
+    
+    },
+    {
+        "name": "CLIFF MOUNTAIN",
+        "elevation": 3960,
+        "ascent": 2160,
+        "route": 17.2,
+        "difficulty": 7,
+        
+    },
+    {
+        "name": "NYE MOUNTAIN",
+        "elevation": 3895,
+        "ascent": 1844,
+        "route": 7.5,
+        "difficulty": 6,
+        
+    },
+    {
+        "name": "COUCHSACHRAGA PEAK",
+        "elevation": 3820,
+        "ascent": 3140,
+        "route": 15,
+        "difficulty": 6,
+    
+    }
+    
+];
+
+window.addEventListener('load', () => {
     init();
 })
 
-
-const init = function(){
-
+const init = function() {
     const dataShelf = document.getElementById("datashelf")
-
-    data.map(function(kristen){
+    data.map(function(kristen) {
         const dataItem = buildDataItem(kristen);
-        dataShelf.innerHTML = dataShelf.innerHTML + dataItem;
+        dataShelf.appendChild(dataItem);
     })
 }
 
-const buildDataItem = function(kristen){
-    return `
-    <div>
-    <h2>${kristen.name}</h2>
-    <h3>${kristen.arms}</h3>
-    <p class="legs">${kristen.legs}</p>
-    </div>
-    <img src=${kristen.picture}>
-    `
+const buildDataItem = function(kristen) {
+// creating data item div.
+    const dataItem = document.createElement('div');
+    dataItem.classList.add('data-item');
+
+// creating text content div.
+    const textContent = document.createElement('div');
+    textContent.classList.add('text-content');
+
+// creating detail text div to go inside text content.
+    const detailText = document.createElement('div');
+    detailText.classList.add('detail-text');
+
+// creating picture container to hold image and topo and to go inside text content
+    const pictureContainer = document.createElement('div');
+    pictureContainer.classList.add('picture-container');
+
+// creating the elements 
+    const image = document.createElement('img');
+    image.src = kristen.picture;
+    image.alt = kristen.name;
+    image.classList.add('version-one');
+
+    const topo = document.createElement('img');
+    topo.src = kristen.topo;
+    topo.classList.add('version-two');
+    
+
+    const name = document.createElement('p');
+    name.classList.add('name');
+    name.textContent = kristen.name;
+
+    const elevation = document.createElement('p');
+    elevation.textContent = `Elevation: ${kristen.elevation} ft`;
+
+    const ascent = document.createElement('p');
+    ascent.textContent = `Ascent: ${kristen.ascent} ft`;
+
+    const route = document.createElement('p');
+    route.textContent = `Route: ${kristen.route} miles`;
+
+    const difficulty = document.createElement('p');
+    difficulty.textContent = `Difficulty: ${kristen.difficulty}`;
+
+    // Append things to respective containers
+    dataItem.appendChild(textContent);
+    textContent.appendChild(name);
+    textContent.appendChild(pictureContainer);
+    pictureContainer.appendChild(image);
+    pictureContainer.appendChild(topo);
+    textContent.appendChild(detailText);
+    detailText.appendChild(elevation);
+    detailText.appendChild(ascent);
+    detailText.appendChild(route);
+    detailText.appendChild(difficulty);
+    textContent.insertBefore(pictureContainer, detailText)
+   
+    
+    return dataItem;
 }
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Project2
 interaction studio project 2
-changing changing
+changes
