a660414a0114328d598d7991a77bfb4cd6d03ff0
azzarakristen <azzak835@newschool.edu>
2024-03-28 12:26:52 -0400

    finaltouches

--- Project_2/script.js
+++ Project_2/script.js
@@ -7,6 +7,7 @@ const data = [
         "difficulty": 5,
         "picture": "images/marcy.jpg",
         "topo": "images/marcytopo.jpg",
+        "link": "https://www.google.com/maps/place/Mt+Marcy/@44.110544,-74.0230628,11.52z/data=!4m14!1m7!3m6!1s0x4ccadce685a92c9b:0xdf101f7317c5fa20!2sMt+Marcy!8m2!3d44.1125536!4d-73.9234763!16zL20vMDJydHFz!3m5!1s0x4ccadce685a92c9b:0xdf101f7317c5fa20!8m2!3d44.1125536!4d-73.9234763!16zL20vMDJydHFz?entry=ttu"
     },
         {
         "name": "ALGONQUIN PEAK",
@@ -16,6 +17,7 @@ const data = [
         "difficulty": 5,
         "picture": "images/algonquin.jpg",
         "topo": "images/algonquintopo.jpg",
+        "link": "https://www.google.com/maps/place/Algonquin+Peak/@44.1436625,-73.9968354,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccade724ba74b1d:0x9b8dffcc393ac641!8m2!3d44.1436635!4d-73.9865357!16zL20vMDZkZ21s?entry=ttu"
 
     },
     {
@@ -25,7 +27,8 @@ const data = [
         "route": 17.8,
         "difficulty": 7,
         "picture": "images/haystack.jpg",
-        "topo": "images/haystacktopo.png"
+        "topo": "images/haystacktopo.png",
+        "link": "https://www.google.com/maps/place/Mt+Haystack/@44.1056083,-73.9107193,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccadccd914e1ed5:0x7c2a2f40fa585eae!8m2!3d44.1056093!4d-73.9004196!16zL20vMDZnN3E5?entry=ttu"
 
     },
     {
@@ -36,6 +39,7 @@ const data = [
         "difficulty": 7,
         "picture": "images/skylight.jpg",
         "topo": "images/skylighttopo.png",
+        "link": "https://www.google.com/maps/place/Mt+Skylight/@44.0994977,-73.9409983,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccadc948f172cf1:0x8f7b062982ea38bd!8m2!3d44.0994987!4d-73.9306986!16zL20vMDZqel9s?entry=ttu"
 
     },
     {
@@ -46,6 +50,7 @@ const data = [
         "difficulty": 4,
         "picture": "images/whiteface.jpg",
         "topo": "images/whitefacetopo.png",
+        "link": "https://www.google.com/maps/place/Whiteface+Mountain/@44.3658794,-73.9129453,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccafdcc8557d667:0x81d6ea6aeba0376b!8m2!3d44.3658804!4d-73.9026456!16zL20vMDZxdndn?entry=ttu"
 
     },
     {
@@ -56,6 +61,7 @@ const data = [
         "difficulty": 7,
         "picture": "images/dix.jpg",
         "topo": "images/dixtopo.png",
+        "link": "https://www.google.com/maps/place/Dix+Mountain/@44.0822746,-73.7965468,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccac16a3832e1d5:0xa3a11ee2c6cf3682!8m2!3d44.0822756!4d-73.7862471!16zL20vMDZ6Zzkx?entry=ttu"
 
     },
     {
@@ -66,6 +72,7 @@ const data = [
         "difficulty": 7,
         "picture": "images/gray.jpg",
         "topo": "images/graytopo.png",
+        "link": "https://www.google.com/maps/place/Gray+Peak/@44.1114417,-73.9451655,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccadcfa78a1e813:0xcd15168ae0b7afdc!8m2!3d44.1114427!4d-73.9348658!16s%2Fm%2F0279_sg?entry=ttu"
 
     },
     {
@@ -76,6 +83,7 @@ const data = [
         "difficulty": 6,
         "picture": "images/iroquois.jpg",
         "topo": "images/iroquoistopo.jpg",
+        "link": "https://www.google.com/maps/place/Iroquois+Peak/@44.1369963,-74.0085023,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccade65caa25859:0xbd07a9fe575ab987!8m2!3d44.1369973!4d-73.9982026!16zL20vMDd3NWNk?entry=ttu"
 
     },
     {
@@ -86,6 +94,7 @@ const data = [
         "difficulty": 6,
         "picture": "images/basin.jpg",
         "topo": "images/basintopo.png",
+        "link": "https://www.google.com/maps/place/Basin+Mountain/@44.1211629,-73.8965524,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccac2d2da08de71:0x9f0f50da9031c364!8m2!3d44.1211639!4d-73.8862527!16s%2Fm%2F02vzr9f?entry=ttu"
 
     },
     {
@@ -96,6 +105,7 @@ const data = [
         "difficulty": 5,
         "picture": "images/gothics.jpg",
         "topo": "images/gothicstopo.png",
+        "link": "https://www.google.com/maps/place/Gothics/@44.1281069,-73.8673846,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccac2eaabc03fdb:0x5dbd6ea3b5bafe2c!8m2!3d44.1281079!4d-73.8570849!16s%2Fm%2F0287y1z?entry=ttu"
 
     },
     {
@@ -106,6 +116,7 @@ const data = [
         "difficulty": 5,
         "picture": "images/colden.jpg",
         "topo": "images/coldentopo.png",
+        "link": "https://www.google.com/maps/place/Mt+Colden/@44.1269966,-73.970167,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccaddb5834d601d:0x81753dbbd14bfefb!8m2!3d44.1269976!4d-73.9598673!16zL20vMGNkbXNj?entry=ttu"
         
     },
     {
@@ -116,6 +127,7 @@ const data = [
         "difficulty": 4,
         "picture": "images/giant.jpg",
         "topo": "images/gianttopo.jpg",
+        "link": "https://www.google.com/maps/place/Giant+Mountain/@44.1612871,-73.7307716,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccaea52324d6a7d:0xea029ee5b162091c!8m2!3d44.1612881!4d-73.7204719!16s%2Fm%2F02872n8?entry=ttu"
         
     },
     {
@@ -126,6 +138,7 @@ const data = [
         "difficulty": 5,
         "picture": "images/nippletop.jpg",
         "topo": "images/nippletoptopo.png",
+        "link": "https://www.google.com/maps/place/Nippletop/@44.0894023,-73.854744,14z/data=!3m1!4b1!4m6!3m5!1s0x4ccac3c355049c11:0x6263ab54cdb09597!8m2!3d44.0894095!4d-73.8157755!16s%2Fm%2F03cqv72?entry=ttu"
     
     },
     {
@@ -136,6 +149,7 @@ const data = [
         "difficulty": 5,
         "picture": "images/santanoni.jpg",
         "topo": "images/santanonitopo.png",
+        "link": "https://www.google.com/maps/place/Santanoni+Peak/@44.0822801,-74.1412852,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccb2771c630bf21:0xefebaa85b56c853a!8m2!3d44.0822811!4d-74.1309855!16s%2Fm%2F03cvcsq?entry=ttu"
 
     },
     {
@@ -146,6 +160,7 @@ const data = [
         "difficulty": 6,
         "picture": "images/redfield.jpg",
         "topo": "images/redfieldtopo.png",
+        "link": "https://www.google.com/maps/place/Mt+Redfield/@44.094776,-73.9601657,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccadc8629c041e9:0x7ea09c50823d122c!8m2!3d44.094777!4d-73.949866!16s%2Fm%2F03csd_f?entry=ttu"
 
     },
     {
@@ -156,6 +171,7 @@ const data = [
         "difficulty": 4,
         "picture": "images/wright.jpg",
         "topo": "images/wrighttopo.png",
+        "link": "https://www.google.com/maps/place/Wright+Peak/@44.1517177,-73.9904465,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccade0e958deccd:0xf53d4cf46f6d2eff!8m2!3d44.1517187!4d-73.9801468!16zL20vMGQxajZt?entry=ttu"
 
     },
     {
@@ -166,6 +182,7 @@ const data = [
         "difficulty": 5,
         "picture": "images/saddleback.jpg",
         "topo": "images/saddlebacktopo.png",
+        "link": "https://www.google.com/maps/place/Saddleback+Mountain/@44.1264342,-73.9138319,14z/data=!3m1!4b1!4m6!3m5!1s0x4ccac2c92c130ed1:0xc9c81d8466fe62d7!8m2!3d44.1264414!4d-73.8748634!16s%2Fm%2F03csn6p?entry=ttu"
     
     },
     {
@@ -176,6 +193,7 @@ const data = [
         "difficulty": 6,
         "picture": "images/panther.jpg",
         "topo": "images/panthertopo.png",
+        "link": "https://www.google.com/maps/place/Panther+Peak/@44.0983843,-74.1710654,14z/data=!3m1!4b1!4m6!3m5!1s0x4ccb2738bf5a1e07:0x73c9aa4023cc8ae7!8m2!3d44.0983915!4d-74.1320969!16s%2Fm%2F03ctyq2?entry=ttu"
 
     },
     {
@@ -186,6 +204,7 @@ const data = [
         "difficulty": 5,
         "picture": "images/tabletop.jpg",
         "topo": "images/tabletoptopo.png",
+        "link": "https://www.google.com/maps/place/Table+Top+Mountain/@44.1402708,-73.9263832,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccadd726a90c2cf:0xc93f6464b2b69e08!8m2!3d44.1402718!4d-73.9160835!16s%2Fm%2F03crsp6?entry=ttu"
 
     },
     {
@@ -196,6 +215,7 @@ const data = [
         "difficulty": 5,
         "picture": "images/rockypeakridge.jpg",
         "topo": "images/rockypeakridgetopo.png",
+        "link": "https://www.google.com/maps/place/Rocky+Peak+Ridge/@44.154443,-73.7158557,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccaeafa3e333821:0x3fedd94c1f4d9b6d!8m2!3d44.154444!4d-73.705556!16s%2Fm%2F028788k?entry=ttu"
 
     },
     {
@@ -206,6 +226,7 @@ const data = [
         "difficulty": 5,
         "picture": "images/macomb.jpg",
         "topo": "images/macombtopo.png",
+        "link": "https://www.google.com/maps/place/Macomb+Mountain/@44.0517201,-73.7904345,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccac6d0abd35261:0x4079676260fb7208!8m2!3d44.0517211!4d-73.7801348!16s%2Fm%2F03cr6zd?entry=ttu"
 
     },
     {
@@ -216,6 +237,7 @@ const data = [
         "difficulty": 5,
         "picture": "images/armstrong.jpg",
         "topo": "images/armstrongtopo.png",
+        "link": "https://www.google.com/maps/place/Armstrong+Mountain/@44.1347731,-73.8593288,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccac29719e3de71:0x4a7bc4b0ba1fe322!8m2!3d44.1347741!4d-73.8490291!16s%2Fm%2F03cshm1?entry=ttu"
 
     },
     {
@@ -226,6 +248,7 @@ const data = [
         "difficulty": 6,
         "picture": "images/hough.jpg",
         "topo": "images/houghtopo.png",
+        "link": "https://www.google.com/maps/place/Hough+Peak/@44.069497,-73.7879349,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccac13072ff8ec3:0xd717162d4ac0d90d!8m2!3d44.069498!4d-73.7776352!16s%2Fm%2F03crjfz?entry=ttu"
 
     },
     {
@@ -236,6 +259,7 @@ const data = [
         "difficulty": 7,
         "picture": "images/seward.jpg",
         "topo": "images/sewardtopo.png",
+        "link": "https://www.google.com/maps/place/Seward+Mountain/@44.1597726,-74.2382924,14z/data=!3m1!4b1!4m6!3m5!1s0x4ccb224a95154a6b:0x845fa535c97d7b6e!8m2!3d44.1597798!4d-74.1993239!16s%2Fm%2F03cvr66?entry=ttu"
     
     },
     {
@@ -246,6 +270,8 @@ const data = [
         "difficulty": 6,
         "picture": "images/marshall.jpg",
         "topo": "images/marshalltopo.png",
+        "link": "https://www.google.com/maps/place/Mt+Marshall/@44.1275528,-74.0221139,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccadef4ac5bd13b:0xb117d605ddbb823c!8m2!3d44.1275538!4d-74.0118142!16s%2Fm%2F03ctmmw?entry=ttu"
+
 
     },
     {
@@ -256,6 +282,7 @@ const data = [
         "difficulty": 7,
         "picture": "images/allen.jpg",
         "topo": "images/allentopo.png",
+        "link": "https://www.google.com/maps/place/Allen+Mountain/@44.070882,-73.9785556,14z/data=!3m1!4b1!4m6!3m5!1s0x4ccadb42396c3fe9:0x97a05fa105d2a3e1!8m2!3d44.0708892!4d-73.9395871!16s%2Fm%2F03cs916?entry=ttu"
 
     },
     {
@@ -266,6 +293,7 @@ const data = [
         "difficulty": 4,
         "picture": "images/bigslide.jpg",
         "topo": "images/bigslidetopo.png",
+        "link": "https://www.google.com/maps/place/Big+Slide+Mountain/@44.1822711,-73.8807199,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccae7cc0459eb77:0xff98c707b1d2ad4a!8m2!3d44.1822721!4d-73.8704202!16s%2Fm%2F02vzrfx?entry=ttu"
 
     },
     {
@@ -276,6 +304,7 @@ const data = [
         "difficulty": 4,
         "picture": "images/esther.jpg",
         "topo": "images/esthertopo.png",
+        "link": "https://www.google.com/maps/place/Esther+Mountain/@44.3869906,-73.900167,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccafe7d34cfbd33:0xf95fe567474e0c60!8m2!3d44.3869916!4d-73.8898673!16s%2Fm%2F03clhx3?entry=ttu"
 
     },
     {
@@ -286,6 +315,7 @@ const data = [
         "difficulty": 5,
         "picture": "images/upper.jpg",
         "topo": "images/uppertopo.png",
+        "link": "https://www.google.com/maps/place/Upper+Wolfjaw+Mountain/@44.1406062,-73.8554398,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccac2994ade6899:0xb95afe88488f4798!8m2!3d44.1406072!4d-73.8451401!16s%2Fm%2F03ctglm?entry=ttu"
     
     },
     {
@@ -296,6 +326,7 @@ const data = [
         "difficulty": 4,
         "picture": "images/lower.jpg",
         "topo": "images/lowertopo.png",
+        "link": "https://www.google.com/maps/place/Lower+Wolfjaw+Mountain/@44.1483835,-73.8429396,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccae82f5508535d:0x2d87afc4cd94f1a9!8m2!3d44.1483845!4d-73.8326399!16s%2Fm%2F03ctg34?entry=ttu"
 
     },
     {
@@ -306,6 +337,7 @@ const data = [
         "difficulty": 6,
         "picture": "images/streetandnye.jpg",
         "topo": "images/streettopo.png",
+        "link": "https://www.google.com/maps/place/Street+Mountain/@44.1709151,-74.0334462,13.95z/data=!4m6!3m5!1s0x4ccae1f1a163f1e1:0xbc01064026259264!8m2!3d44.1796954!4d-74.0277122!16s%2Fg%2F11ghbkf9kv?entry=ttu"
 
     },
     {
@@ -316,6 +348,7 @@ const data = [
         "difficulty": 5,
         "picture": "images/phelps.jpg",
         "topo": "images/phelpstopo.png",
+        "link": "https://www.google.com/maps/place/Phelps+Mountain/@44.1497977,-73.9301521,13.71z/data=!4m6!3m5!1s0x4ccae77e32cf33eb:0xa784cf2af4ce6afa!8m2!3d44.1569957!4d-73.9209775!16s%2Fm%2F03crs62?entry=ttu"
     
     },
     {
@@ -326,6 +359,7 @@ const data = [
         "difficulty": 7,
         "picture": "images/donaldson.jpg",
         "topo": "images/donaldsontopo.png",
+        "link": "https://www.google.com/maps/place/Donaldson+Mountain/@44.1539461,-74.2212907,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccb23b1a6ebc841:0x16aff708e95c910!8m2!3d44.1539471!4d-74.210991!16s%2Fm%2F03cvdn5?entry=ttu"
     
     },
     {
@@ -336,6 +370,7 @@ const data = [
         "difficulty": 6,
         "picture": "images/seymour.jpg",
         "topo": "images/seymourtopo.png",
+        "link": "https://www.google.com/maps/place/Seymour+Mountain/@44.1578337,-74.1824,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccb218093eaf281:0xa313d29bc8c0c4eb!8m2!3d44.1578347!4d-74.1721003!16s%2Fm%2F03cvwxr?entry=ttu"
 
     },
     {
@@ -346,6 +381,7 @@ const data = [
         "difficulty": 4,
         "picture": "images/sawteeth.jpg",
         "topo": "images/sawteethtopo.png",
+        "link": "https://www.google.com/maps/place/Sawteeth/@44.113332,-73.8609667,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccac2fc82aa2231:0x7ec712682abd8925!8m2!3d44.113333!4d-73.850667!16s%2Fm%2F03csqf1?entry=ttu"
 
     },
     {
@@ -356,6 +392,7 @@ const data = [
         "difficulty": 2,
         "picture": "images/cascade.jpg",
         "topo": "images/cascadetopo.png",
+        "link": "https://www.google.com/maps/place/Cascade+Mountain/@44.2186584,-73.8704426,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccae67464f00141:0x24444f8724613725!8m2!3d44.2186594!4d-73.8601429!16zL20vMDM1aHM0?entry=ttu"
 
     },
     {
@@ -366,6 +403,7 @@ const data = [
         "difficulty": 6,
         "picture": "images/southdix.jpg",
         "topo": "images/southdixtopo.png",
+        "link": "https://www.google.com/maps/place/South+Dix/@44.060053,-73.7846011,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccac6d4ad27de37:0x86083c540fa802e6!8m2!3d44.060054!4d-73.7743014!16s%2Fm%2F03cr5xt?entry=ttu"
 
     },
     {
@@ -376,6 +414,7 @@ const data = [
         "difficulty": 3,
         "picture": "images/porter.jpg",
         "topo": "images/portertopo.png",
+        "link": "https://www.google.com/maps/place/Porter+Mountain/@44.2153191,-73.8824439,14z/data=!3m1!4b1!4m6!3m5!1s0x4ccae605deb86e33:0x18f9712a70d4f64c!8m2!3d44.2153262!4d-73.8434754!16zL20vMDU3MGZm?entry=ttu"
 
     },
     {
@@ -386,6 +425,7 @@ const data = [
         "difficulty": 4,
         "picture": "images/colvin.jpg",
         "topo": "images/colvintopo.png",
+        "link": "https://www.google.com/maps/place/Mt+Colvin/@44.0939414,-73.8448826,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccac3bbbac76b45:0x76cb054b54d3b1bf!8m2!3d44.0939424!4d-73.8345829!16zL20vMGNkbXRy?entry=ttu"
 
     },
     {
@@ -396,6 +436,7 @@ const data = [
         "difficulty": 7,
         "picture": "images/emmons.jpg",
         "topo": "images/emmonstopo.png",
+        "link": "https://www.google.com/maps/place/Mt+Emmons/@44.1436626,-74.2530149,14z/data=!3m1!4b1!4m6!3m5!1s0x4ccb23bdd0125a07:0xa7f2b56324992866!8m2!3d44.1436698!4d-74.2140464!16s%2Fm%2F03cvh94?entry=ttu"
 
     },
     {
@@ -406,6 +447,7 @@ const data = [
         "difficulty": 5,
         "picture": "images/dial.jpg",
         "topo": "images/dialtopo.png",
+        "link": "https://www.google.com/maps/place/Dial+Mountain/@44.1058848,-73.8062699,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccac229849b2a4b:0x42286537d6ce2e28!8m2!3d44.1058858!4d-73.7959702!16s%2Fm%2F03cql5k?entry=ttu"
 
     },
     {
@@ -416,6 +458,7 @@ const data = [
         "difficulty": 6,
         "picture": "images/grace.jpg",
         "topo": "images/gracetopo.png",
+        "link": "https://www.google.com/maps/place/Grace+Peak/@44.06533,-73.7676557,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccac0d0e824134d:0x75b124c097ea1308!8m2!3d44.065331!4d-73.757356!16s%2Fm%2F03cr50w?entry=ttu"
 
     },
     {
@@ -426,6 +469,7 @@ const data = [
         "difficulty": 4,
         "picture": "images/blake.jpg",
         "topo": "images/blaketopo.png",
+        "link": "https://www.google.com/maps/place/Blake+Peak/@44.081442,-73.8548827,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccac39066236573:0x30f45539871a3039!8m2!3d44.081443!4d-73.844583!16s%2Fm%2F03cq7y9?entry=ttu"
     
     },
     {
@@ -436,6 +480,7 @@ const data = [
         "difficulty": 7,
         "picture": "images/cliff.jpg",
         "topo": "images/clifftopo.png",
+        "link": "https://www.google.com/maps/place/Cliff+Mountain/@44.103324,-73.9800656,17z/data=!3m1!4b1!4m6!3m5!1s0x4ccadc14a64b75ff:0xe1007d57e4ba675c!8m2!3d44.1033241!4d-73.9751947!16s%2Fg%2F11gd393_vr?entry=ttu"
         
     },
     {
@@ -446,6 +491,7 @@ const data = [
         "difficulty": 6,
         "picture": "images/streetandnye.jpg",
         "topo": "images/nyetopo.png",
+        "link": "https://www.google.com/maps/place/Nye+Mountain/@44.1872021,-74.0266368,17z/data=!3m1!4b1!4m6!3m5!1s0x4ccae06bb6884833:0xb8d0a6a459e4cbb9!8m2!3d44.1872021!4d-74.0240619!16s%2Fg%2F11hd9h84c5?entry=ttu"
         
     },
     {
@@ -456,6 +502,7 @@ const data = [
         "difficulty": 6,
         "picture": "images/couch.jpg",
         "topo": "images/couchtopo.png",
+        "link": "https://www.google.com/maps/place/Couchsachraga+Peak/@44.0956136,-74.1704536,15z/data=!3m1!4b1!4m6!3m5!1s0x4ccb26e067890e77:0x2a27e858eda41646!8m2!3d44.0956146!4d-74.1601539!16s%2Fm%2F03ctxb_?entry=ttu"
     
     }
     
@@ -478,7 +525,8 @@ const buildDataItem = function (kristen) {
     dataItem.classList.add('data-item');
     dataItem.innerHTML = `
         <div class="text-content">
-            <p class="name">${kristen.name}</p>
+        <a href="${kristen.link}">
+            <p class="name">${kristen.name}</p></a>
             <div class="picture-container">
                 <img src="${kristen.picture}" alt="${kristen.name}" class="version-one" />
                 <div class="topo-map">
@@ -489,7 +537,7 @@ const buildDataItem = function (kristen) {
                 <p>elevation: ${kristen.elevation} ft</p>
                 <p>ascent: ${kristen.ascent} ft</p>
                 <p>route: ${kristen.route} mi</p>
-                <p>difficulty: ${kristen.difficulty}</p>
+                <p>difficulty: ${kristen.difficulty}/7</p>
             </div>
         </div>`;
     return dataItem;
